2e606733d4b7e78d6adfbd5a58a744323d23a91d
Mats Faugli <mats.faugli@if.no>
2014-09-24 22:59:57 +0200

    Remove created by comment

--- plugin/src/main/groovy/com/github/fowlie/GradleJenkinsTestPlugin.groovy
+++ plugin/src/main/groovy/com/github/fowlie/GradleJenkinsTestPlugin.groovy
@@ -3,13 +3,9 @@ package com.github.fowlie
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 
-/**
- * Created by mfa on 22.09.2014.
- */
 class GradleJenkinsTestPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
-        println 'hello plugin'
         project.apply(plugin: 'java')
         project.task('jenkinsTest', type: GradleJenkinsTestTask) << {
             description 'Updates the test report timestamps, so Jenkins doesn\'t mark the job as failed.'

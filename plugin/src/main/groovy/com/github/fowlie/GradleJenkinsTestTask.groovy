2816b21d9630f23f4c842abb208803140faad006
Mats Faugli <mats.faugli@if.no>
2014-09-24 23:01:44 +0200

    Remove created by comment, input files and doLast closure

--- plugin/src/main/groovy/com/github/fowlie/GradleJenkinsTestTask.groovy
+++ plugin/src/main/groovy/com/github/fowlie/GradleJenkinsTestTask.groovy
@@ -3,18 +3,12 @@ package com.github.fowlie
 import org.gradle.api.DefaultTask
 import org.gradle.api.tasks.TaskAction
 
-/**
- * Created by mfa on 22.09.2014.
- */
 class GradleJenkinsTestTask extends DefaultTask {
     @TaskAction
     def jenkinsTest() {
-        inputs.files test.outputs.files
-        doLast {
-            def testResults = test.getReports().getJunitXml().getDestination()
+            def testResults = project.test.getReports().getJunitXml().getDestination()
             if (testResults.exists()) testResults.eachFile {
                 it.lastModified = System.currentTimeMillis()
             }
-        }
     }
 }
